09ad9815b15262158e7e47856ca60e4b2f731063
WellingtonIdeao <wellington.ideao@gmail.com>
2021-05-16 17:53:06 -0300

    add models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+
+class ImageProjectInline(admin.TabularInline):
+    model = ImageProject
+    extra = 0
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ImageProjectInline]
+
+
+admin.site.register(Contact)
+admin.site.register(Profile)
+admin.site.register(Service)
+admin.site.register(Client)
+admin.site.register(Project, ProjectAdmin)
+
+
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,66 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    subject = models.CharField(max_length=50, blank=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    b_date = models.DateField()
+    occupation = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=11)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    city = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date = models.DateField()
+    url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ImageProject(models.Model):
+    name = models.CharField(max_length=50, default='image')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
+
+
