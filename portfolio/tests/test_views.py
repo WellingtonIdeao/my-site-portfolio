5f57f692a84bc99220ddcb51b4ce9aec94012638
WellingtonIdeao <wellington.ideao@gmail.com>
2021-05-20 18:14:14 -0300

    add profile in context in about.html template

--- portfolio/tests/test_views.py
+++ portfolio/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.shortcuts import reverse
+from ..models import Profile
+from django.utils import timezone
 
 
 class IndexViewTests(TestCase):
@@ -11,9 +13,28 @@ class IndexViewTests(TestCase):
 
 class AboutViewTests(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        Profile.objects.create(
+            name='name',
+            description='description',
+            b_date=timezone.now(),
+            occupation='occupation',
+            email='email@mail.com',
+            phone='99999999999',
+        )
+
     def test_status_200(self):
         response = self.client.get(reverse('portfolio:about'))
         self.assertEqual(response.status_code, 200)
+
+    def test_profile_on_context(self):
+        profile = Profile.objects.get(pk=1)
+        response = self.client.get(reverse('portfolio:about'))
+        self.assertEqual(
+            response.context['profile'],
+            profile,
+        )
 
 
 class ServicesViewTests(TestCase):
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404
+from .models import Profile
 # Create your views here.
 
 
@@ -9,6 +11,11 @@ class IndexView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'portfolio/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = get_object_or_404(Profile, pk=1)
+        return context
 
 
 class ServicesView(TemplateView):
