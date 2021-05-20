f3ca4b97afda8f2a6039816ca583a5184d232683
WellingtonIdeao <wellington.ideao@gmail.com>
2021-05-20 13:14:22 -0300

    add url templateviews in portfolio.urls and templates .html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from django.views.generic.base import TemplateView
+
 app_name = 'portfolio'
 urlpatterns = [
-    path('', index, name='index'),
+    path('', TemplateView.as_view(template_name='portfolio/index.html'), name='index'),
+    path('about/', TemplateView.as_view(template_name='portfolio/about.html'), name='about'),
+    path('services/', TemplateView.as_view(template_name='portfolio/services.html'), name='services'),
+    path('portfolio/', TemplateView.as_view(template_name='portfolio/portfolio.html'), name='portfolio'),
+    path('contact/', TemplateView.as_view(template_name='portfolio/contact.html'), name='contact'),
 ]
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request, template_name='portfolio/index.html'):
-    return render(request, template_name, {})
