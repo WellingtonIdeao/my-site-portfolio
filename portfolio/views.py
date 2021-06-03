1e957f63a7aa435f93a067b42777855a525d90bf
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-02 23:09:30 -0300

    fix bootstrap5 grid system on service page

--- portfolio/views.py
+++ portfolio/views.py
@@ -15,12 +15,10 @@ class IndexView(TemplateView):
 class AboutView(TemplateView):
     template_name = 'portfolio/about.html'
 
-    """
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['profile'] = get_object_or_404(Profile, pk=1)
         return context
-    """
 
 
 class ServicesView(ListView):
