f49da20c7e916f514abb84d0927763ccc6c1a040
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-11 17:54:56 -0300

    remove config vars value on heroku settings

--- mysite/settings/heroku.py
+++ mysite/settings/heroku.py
@@ -1,22 +1,4 @@
 from .base import *
-import json
-
-
-# Extract Keys From the Json file
-def get_key(path):
-    with open(path) as f:
-        return json.load(f)
-
-
-keys = get_key(BASE_DIR / 'secrets.json')
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = keys['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = keys['DEBUG']
-
-ALLOWED_HOSTS = keys['ALLOWED_HOSTS']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -28,19 +10,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-# HSTS is an HTTP header that informs a browser that all future connections to a particular site should always use HTTPS
-SECURE_HSTS_SECONDS = keys['SECURE_HSTS_SECONDS']
-SECURE_HSTS_PRELOAD = keys['SECURE_HSTS_PRELOAD']
-SECURE_HSTS_INCLUDE_SUBDOMAINS = keys['SECURE_HSTS_INCLUDE_SUBDOMAINS']
-
-# Requests over HTTP are redirected to HTTPS.
-SECURE_SSL_REDIRECT = keys['SECURE_SSL_REDIRECT']
-
-# Secure cookies
-SESSION_COOKIE_SECURE = keys['SESSION_COOKIE_SECURE']
-CSRF_COOKIE_SECURE = keys['CSRF_COOKIE_SECURE']
-
 
 # The file storage engine to use when collecting static files with the collectstatic management command.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
