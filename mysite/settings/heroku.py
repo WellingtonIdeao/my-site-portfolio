5be1b1fdcc20713dc6af4a28ee32a81060be5880
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-11 17:42:36 -0300

    add heroku settings

---
+++ mysite/settings/heroku.py
@@ -0,0 +1,16 @@
+from .base import *
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# The file storage engine to use when collecting static files with the collectstatic management command.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
