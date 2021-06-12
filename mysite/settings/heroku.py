a9bee6e2a27426ad727c85222418cd9af23b2a82
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-12 12:18:01 -0300

    fix heroku settings 0.2

--- mysite/settings/heroku.py
+++ mysite/settings/heroku.py
@@ -6,6 +6,20 @@ SECRET_KEY = ")cs-%s3ednl%7cw2$12&g-h(%*c8lv+!zcw2$jj$^*t7wm+^$%"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "darde7b71sag6t",
+        'USER': "xsdyaptsklaroz",
+        'PASSWORD': "d3d74b6616053c4bf48a3d4ed25ab785313b3f07cce9ecfbfd0ba58192120fe1",
+        'HOST': "ec2-3-233-7-12.compute-1.amazonaws.com",
+        'PORT': 5432,
+    }
+}
+
 # The file storage engine to use when collecting static files with the collectstatic management command.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
