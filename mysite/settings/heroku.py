8516a8708f310bf5f5ef60a9d5b35ae3649c2165
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-12 12:12:24 -0300

    fix heroku settings 0.1

--- mysite/settings/heroku.py
+++ mysite/settings/heroku.py
@@ -1,3 +1,11 @@
-from .production import *
+from .base import *
 
-SECRET_KEY=")cs-%s3ednl%7cw2$12&g-h(%*c8lv+!zcw2$jj$^*t7wm+^$%"
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ")cs-%s3ednl%7cw2$12&g-h(%*c8lv+!zcw2$jj$^*t7wm+^$%"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# The file storage engine to use when collecting static files with the collectstatic management command.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
