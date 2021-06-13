e6033910d4577b309b89ff4b0f977bf2e48dc873
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-13 11:26:57 -0300

    fix heroku

--- mysite/settings/heroku.py
+++ mysite/settings/heroku.py
@@ -22,16 +22,17 @@ DATABASES = {
 }
 
 # HSTS is an HTTP header that informs a browser that all future connections to a particular site should always use HTTPS
-SECURE_HSTS_SECONDS = 3600
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = env.int('SECURE_HSTS_SECONDS')
+SECURE_HSTS_PRELOAD = env.bool('SECURE_HSTS_PRELOAD')
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('SECURE_HSTS_INCLUDE_SUBDOMAINS')
+
 
 # Requests over HTTP are redirected to HTTPS.
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = env.bool('SECURE_SSL_REDIRECT')
 
 # Secure cookies
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = env.bool('SESSION_COOKIE_SECURE')
+CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE')
 
 
 # The file storage engine to use when collecting static files with the collectstatic management command.
